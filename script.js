5080b49a751d03007ec5209cfa74d4f91b217ee3
abhirup-basu-gh <abhirup.basu@growhut.in>
2026-09-16 00:09:39 +0530

    update commit

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ xhr.addEventListener('readystatechange', function () {
 	}
 });
 
-xhr.open('GET', 'https://open-weather13.p.rapidapi.com/fivedaysforcast?latitude=40.730610&longitude=-73.935242&lang=EN');
+xhr.open('GET', 'https://cities-temperature.p.rapidapi.com/weather/v1/current?location=Kolkata');
 xhr.setRequestHeader('x-rapidapi-key', 'fb7ce7be52msh1a19cce97d3b21ep1c2a7cjsn96c2be0826f1');
-xhr.setRequestHeader('x-rapidapi-host', 'open-weather13.p.rapidapi.com');
+xhr.setRequestHeader('x-rapidapi-host', 'cities-temperature.p.rapidapi.com');
 xhr.setRequestHeader('Content-Type', 'application/json');
 
 xhr.send(data);
